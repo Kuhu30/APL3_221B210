4422c30dd1609c308f3ae75faa7c89ef10fcb4ac
221B210 <221B210@juet.co.in>
2025-01-31 14:36:35 +0530

    This is Lab Exercise 2

---
+++ le2/problem2/Voice.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Voice{
+    void prepareVoice(){
+        Animal[] animals = new Animal[5];
+        animals[0] = new Cow();
+        animals[1] = new Dog();
+        animals[2] = new Pig();
+        animals[3] = new Goat();
+        animals[4] = new Lion();
+        hear (animals);
+    }
+    void hear(Animal[] animals) {
+        for (int i=0;i<5;i++) {
+            animals[i].makeVoice();  
+        }
+    }
+}
+
+
+
